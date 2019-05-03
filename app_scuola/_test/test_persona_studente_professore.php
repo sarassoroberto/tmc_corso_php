3ca05ea0b1c8bdf426a1f1e86fd066ff4e50c212
sarassoroberto <sarassoroberto@gmail.com>
2019-05-03 23:51:55 +0200

    creazione di un costruttore per la classe Persona

--- app_scuola/_test/test_persona_studente_professore.php
+++ app_scuola/_test/test_persona_studente_professore.php
@@ -4,12 +4,7 @@ include '../class/Studente.php';
 include '../class/Professore.php';
 
 /** Creo un istanza di Studente */
-$studente  =  new Studente();
-
-$studente->nome = "Marco";
-$studente->cognome = "Rossi";
-$studente->classe = 2;
-$studente->sezione = "B";
+$studente  =  new Studente('Marco', 'Rossi', 2, 'B');
 
 $studente->email = "marco.rossi@email.edu";
 
@@ -17,12 +12,7 @@ print_r($studente);
 
 
 /** Creo un istanza di Professore */
-$professore = new Professore();
-
-$professore->nome = "Giovanni";
-$professore->cognome = "Sbattuti";
-$professore->classe = 2;
-$professore->sezione = "B";
+$professore = new Professore('Giovanni', 'Sbattuti', 2, 'B');
 
 $professore->email = "giovanni.sbattuti@email.edu";
 
--- app_scuola/class/Persona.php
+++ app_scuola/class/Persona.php
@@ -6,4 +6,12 @@ class Persona
     public $classe;
     public $sezione;
     public $email;
+
+    public function __construct($nome, $cognome, $classe, $sezione)
+    {
+        $this->nome = $nome;
+        $this->cognome = $cognome;
+        $this->classe = $classe;
+        $this->sezione = $sezione;
+    }
 }
