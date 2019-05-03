9cac3b23e742cf40143db3809989758fb289aa33
sarassoroberto <sarassoroberto@gmail.com>
2019-05-03 22:21:27 +0200

    Aggiunta classe Studente - solo proprietà -

--- app_scuola/class/Studente.php
+++ app_scuola/class/Studente.php
@@ -5,21 +5,6 @@ class Studente
     public $nome;
     public $cognome;
     public $voti = array();
-
-    /** metodo */
-    public function aggiungiVoto($voto)
-    {
-        /** apri database trova studente e assegna nuovo voto */
-        /** $array[] = 10 */
-        /** $this indica che state usando i voti dell'istanza dello studente*/
-        $this->voti[] = $voto;
-    }
-
-    public function ottieniMedia()
-    {
-        $somma_voti  = array_sum($this->voti);
-        $numero_di_voti = count($this->voti);
-        $media = $somma_voti / $numero_di_voti;
-        return $media;
-    }
+    public $classe;
+    public $sezione;
 }
