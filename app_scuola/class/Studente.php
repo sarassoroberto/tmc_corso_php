94f6deb409033b05529261793aa47df20f1ac2da
roberto.sarasso <sarassoroberto@gmail.com>
2019-05-04 11:40:12 +0200

    Studente, inseriti medodi aggiungiVoto, ottieniMedia()

--- app_scuola/class/Studente.php
+++ app_scuola/class/Studente.php
@@ -7,5 +7,19 @@ class Studente extends Persona
 {
     // proprietà e  i metodi che riguardano lo Studente
     public $voti = array();
+
+     /* metodo */
+     public function aggiungiVoto($voto)
+     {
+         /* $this indica che state usando i voti dell'istanza dello studente*/
+         $this->voti[] = $voto;
+     }
+ 
+     public function ottieniMedia(){
+         $somma_voti  = array_sum($this->voti);
+         $numero_di_voti = count($this->voti);
+         $media = $somma_voti / $numero_di_voti;
+         return $media;
+     }
     
 }
