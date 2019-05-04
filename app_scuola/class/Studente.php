489b5e6a4a2d99a5257aa53a24db5770779363ea
roberto.sarasso <sarassoroberto@gmail.com>
2019-05-04 09:27:33 +0200

    Creata classe Studente, aggiunte proprietà

---
+++ app_scuola/class/Studente.php
@@ -0,0 +1,10 @@
+<?php
+// Dichiarazione di classe
+class Studente {
+    // proprietà e  i metodi che riguardano lo Studenteù
+    public $nome;
+    public $cognome;
+    public $voti = array();
+    public $classe;
+    public $sezione;
+}
