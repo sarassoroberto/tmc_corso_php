7f0ba08534d4641c2fad0b92924a7b2bed314f9a
roberto.sarasso <sarassoroberto@gmail.com>
2019-05-04 10:17:32 +0200

    Test classe Studente

---
+++ app_scuola/_test/studente.php
@@ -0,0 +1,8 @@
+<?php
+require '../class/Studente.php';
+
+// crea un istanza della classe Studente
+$studente = new Studente();
+
+print_r($studente);
+
--- app_scuola/class/Studente.php
+++ app_scuola/class/Studente.php
@@ -1,7 +1,7 @@
 <?php
 // Dichiarazione di classe
 class Studente {
-    // proprietà e  i metodi che riguardano lo Studenteù
+    // proprietà e  i metodi che riguardano lo Studente
     public $nome;
     public $cognome;
     public $voti = array();
