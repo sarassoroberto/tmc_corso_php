8e431e017bdc885f6f5b3162bde40569963ff2b8
sarassoroberto <sarassoroberto@gmail.com>
2019-05-10 23:05:36 +0200

    inserit getter e setter in persona

--- app_scuola/class/Persona.php
+++ app_scuola/class/Persona.php
@@ -1,11 +1,11 @@
 <?php
 class Persona
 {
-    public $nome;
-    public $cognome;
-    public $classe;
-    public $sezione;
-    public $email;
+    private $nome;
+    private $cognome;
+    private $classe;
+    private $sezione;
+    private $email;
 
     public function __construct($nome, $cognome, $classe, $sezione)
     {
@@ -13,5 +13,85 @@ class Persona
         $this->cognome = $cognome;
         $this->classe = $classe;
         $this->sezione = $sezione;
+    }
+
+    /**
+     * Get the value of nome
+     */
+    public function getNome()
+    {
+        return $this->nome;
+    }
+
+    /**
+     * Set the value of nome
+     *
+     * @return  self
+     */
+    public function setNome($nome)
+    {
+        $this->nome = $nome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of cognome
+     */
+    public function getCognome()
+    {
+        return $this->cognome;
+    }
+
+    /**
+     * Set the value of cognome
+     *
+     * @return  self
+     */
+    public function setCognome($cognome)
+    {
+        $this->cognome = $cognome;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of classe
+     */
+    public function getClasse()
+    {
+        return $this->classe;
+    }
+
+    /**
+     * Set the value of classe
+     *
+     * @return  self
+     */
+    public function setClasse($classe)
+    {
+        $this->classe = $classe;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
     }
 }
