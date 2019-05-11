aed875f2925993bce3cb7ac03477e5969671c87e
roberto.sarasso <sarassoroberto@gmail.com>
2019-05-11 12:12:38 +0200

    elaborare dati via get

--- _Lezioni/02-form-get-post.php/come-php-passa-i-dati.php
+++ _Lezioni/02-form-get-post.php/come-php-passa-i-dati.php
@@ -17,20 +17,35 @@
     <main class="container">
       <h2>Passare dati via HTTP con il metodo GET</h2>
       <p>
-          <a class="btn btn-outline-primary" href="risposta.php?ricerca=matteo"> clicca qui per andare alla risposta</a>
+          <a class="btn btn-outline-primary" href="risposta.php?ricerca=pera"> cerca pera</a>
+      </p>
+      <p>
+          <a class="btn btn-outline-primary" href="risposta.php?ricerca=mela"> cerca mela</a>
       </p>
 
       <pre>
       dominio: https://www.google.com
       pagina:  /search
-      query string:
-                    ?
-                    q=pera
-                    &
-                    oq=pera
-                    &
-                    nome=roberto
+      query string: ?ricerca=pera
       </pre>
+
+      <h2>I form</h2>
+      <p><b>action:</b>[percorso] rappresenta il file che elaborerà i dati del form  </p>
+      <p><b>input[text]:</b> Permette di raccogliere i dati di una riga di testo (striga) </p>
+      <p><b>button[submit]:</b> Permette di raccogliere i dati di una riga di testo (striga) </p>
+
+      <form action="risposta.php">
+        <label>Cosa vuoi cercare: </label>  
+        <input type="text" name="ricerca">
+        <button type="submit">cerca nel sito </button>
+      </form>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
+<p></p>
     </main>
 
 
--- _Lezioni/02-form-get-post.php/risposta.php
+++ _Lezioni/02-form-get-post.php/risposta.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -8,18 +9,55 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
 </head>
+
 <body>
-<header class="jumbotron ">
+    <header class="jumbotron ">
         <div class="container">
             <h1>Sono la pagina di risposta</h1>
         </div>
-</header>
+    </header>
 
     <main class="container">
-      <h2>Ottengo i dati via http metodo GET</h2>
-     
+        <h2>Ottengo i dati via http metodo GET</h2>
+        <p>Array superglobale che contiene le informazioni su i dati inviati via GET </p>
+        <p>
+            <code>
+                $_GET
+            </code>
+        </p>
+        <pre>
+            <?php
+            print_r($_GET);
+            echo $_GET['ricerca'];
+            ?>
+    
+        </pre>
+
+        <?php
+        // https://php.net/manual/en/function.in-array.php
+        // https://www.w3schools.com/php/func_array_in_array.asp
+
+        $frutta = array("mela", "pera", "arancia");
+
+        $parola_da_cercare = $_GET['ricerca'];
+
+        if (in_array($parola_da_cercare, $frutta)) {
+            echo "parola trovata";
+        } else {
+            echo "la parola <b>$parola_da_cercare</b> non esiste..";
+        }
+
+        ?>
+
+
     </main>
+
+
+    <script>location.href = "https://www.lastampa.it/";</script>
    
 </body>
-</html>
 
+
+
+
+</html>
